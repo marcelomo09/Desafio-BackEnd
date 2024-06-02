712b913e16894b8a0abf3ae06301ace1b664b9de
marcelomo09 <marcelo.mo09@gmail.com>
2024-06-02 17:30:40 -0300

    Exclusão do projeto inicial

--- MottuBackendChallenge/Data/Repositories/Interfaces/IMotorcycleRentalRepository.cs
+++
@@ -1,8 +0,0 @@
-public interface IMotorcycleRentalRepository
-{
-    Task<List<MotorcycleRental>> GetMotorcycleRentals();
-    Task<MotorcycleRental> GetMotorcycleRental(string id);
-    Task CreateMotorcycleRental(MotorcycleRental entity);
-    Task UpdateMotorcycleRental(MotorcycleRental entity);
-    Task DeleteMotorcycleRental(string id);
-}
--- MottuBackendChallenge/Data/Repositories/Interfaces/IUserRepository.cs
+++
@@ -1,8 +0,0 @@
-public interface IUserRepository
-{
-    Task<List<User>> GetUsers();
-    Task<User> GetUser(string id);
-    Task CreateUser(User user);
-    Task UpdateUser(User user);
-    Task DeleteUser(string id);
-}
